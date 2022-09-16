647e1230a26437d65648d18145640c57b3936c02
SeoJunhwan <seojunhwan@kakao.com>
2022-09-16 14:19:02 +0900

    docs 예외처리 주석 추가

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   fake_file_name (file name is useless too)          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: 42header-remover <whatever@example.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/05 10:03:17 by junseo            #+#    #+#             */
-/*   Updated: 2022/09/15 15:14:05 by junseo           ###   ########.fr       */
+/*   Created: 1970/01/01 00:00:00 by VCS handles       #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by file history     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	char	*line;
-
-	(void)argc;
-	(void)argv;
-	while (1)
-	{
-		line = readline("hihi : ");
-		printf("%s\n", line);
-	}
 	return (0);
 }
 
+	/*
+		lr parser
+		Lalr parser
+
+    unset <- 환경변수 삭제
+		export <- 환경변수 추가
+		readline 함수 중에서 너무 빠르게 지랄하면 누수가 생긴다 <- 디펜스 잘 해봅시다
+	*/
