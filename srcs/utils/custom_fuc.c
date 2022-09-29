fc0234724c0f621fd14f8a152a401257c2499588
Seojunhwan <seojunhwan@kakao.com>
2022-09-29 17:11:39 +0900

    refactor: create wrappedStrJoin fuc, add bzero

--- srcs/utils/custom_fuc.c
+++ srcs/utils/custom_fuc.c
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 20:04:05 by junseo            #+#    #+#             */
-/*   Updated: 2022/09/25 02:18:17 by junseo           ###   ########.fr       */
+/*   Updated: 2022/09/28 04:53:25 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@ void	*ft_malloc(size_t size, size_t n)
 	void	*ret;
 
 	ret = malloc(size * n);
+	ft_bzero(ret, size * n);
 	if (ret == NULL)
 		exit_with_err("malloc()", strerror(errno), EXIT_FAILURE);
 	return (ret);
+}
+
+char	*ft_strjoin_with_free(char *s1, char *s2)
+{
+	char	*joined_str;
+
+	joined_str = ft_strjoin(s1, s2);
+	free(s1);
+	free(s2);
+	return (joined_str);
 }
