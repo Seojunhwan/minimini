fa2cfa8a835a228b97dadb23a2cfa31436187b82
Seojunhwan <seojunhwan@kakao.com>
2022-10-03 16:32:01 +0900

    fix:fucking red line

--- srcs/utils/exit_utils.c
+++ srcs/utils/exit_utils.c
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 02:09:40 by junseo            #+#    #+#             */
-/*   Updated: 2022/10/03 02:51:00 by junseo           ###   ########.fr       */
+/*   Updated: 2022/10/03 16:31:42 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	exit_with_err(char *msg, char *msg2, int error_no)
 int	parse_error(int option)
 {
 	g_state.exit_status = 1;
-	printf("\e[101;97;1mmini_mini_mini_shell: ");
+	printf("\e[101;97;1mmini_mini_mini_shell:\e[0m ");
 	if (option == 1 || option == 3)
 		g_state.exit_status = 258;
 	printf("%s\e[0m", g_errors[option]);
